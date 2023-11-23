f940c7d4616a829e5f18056757e10d4c7e1a9f09
KevinZucca <kev.94@live.it>
2023-11-23 21:13:31 +0100

    fix: notes

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const postsRouter = require("./routers/posts");
 const errorHandler = require("./middlewares/errorHandler");
 const notFoundRoute = require("./middlewares/notFoundRoute");
 
+// body parser config
 app.use(express.json());
 
 // routes
 app.use("/posts", postsRouter);
 
+// middlewares
 app.use(notFoundRoute);
 app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`server is running on port ${port} `);
 });

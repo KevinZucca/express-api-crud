34752c722dbbdc786da898d7aab9662206be5b1d
KevinZucca <kev.94@live.it>
2023-11-23 20:59:25 +0100

    feat: single post filters

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,9 +2,27 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const { kebabCase } = require("lodash");
 
-// get all posts
+// get all posts or filtered
 exports.index = async (req, res) => {
-  const data = await prisma.post.findMany();
+  const { search, published } = req.query;
+  const posts = {};
+  if (search) {
+    posts.OR = [
+      { title: { contains: search } },
+      { content: { contains: search } },
+    ];
+  }
+  if (published == "true") {
+    const publishedPosts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+    });
+    return res.json(publishedPosts);
+  }
+  const data = await prisma.post.findMany({
+    where: posts,
+  });
   return res.json(data);
 };
 
@@ -32,4 +50,32 @@ exports.create = async (req, res) => {
     },
   });
   res.json(newPost);
+};
+
+// edit a post
+exports.update = async (req, res) => {
+  const data = req.body;
+  const updatedPost = await prisma.post.update({
+    data: {
+      title: data.title,
+      slug: data.slug,
+      image: data.image,
+      content: data.content,
+      published: data.published,
+    },
+    where: {
+      slug: data.slug,
+    },
+  });
+  res.json(updatedPost);
+};
+
+// delete a post
+exports.destroy = async (req, res) => {
+  await prisma.post.delete({
+    where: {
+      slug: req.params.slug,
+    },
+  });
+  res.json("Post eliminato");
 };
--- routers/posts.js
+++ routers/posts.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const postsController = require("../controllers/posts");
 
 router.get("/", postsController.index);
-router.get("/:slug", postsController.show);
 router.post("/", postsController.create);
+router.get("/:slug", postsController.show);
+router.put("/:slug", postsController.update);
+router.delete("/:slug", postsController.destroy);
 
 module.exports = router;

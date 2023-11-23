b08b4680f7db6a25a35563cd0d68e75d4978f8d3
KevinZucca <kev.94@live.it>
2023-11-23 15:01:48 +0100

    feat: create function

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,14 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const { kebabCase } = require("lodash");
 
+// get all posts
+exports.index = async (req, res) => {
+  const data = await prisma.post.findMany();
+  return res.json(data);
+};
+
+// get single post from id
 exports.show = async (req, res) => {
   const data = await prisma.post.findUnique({
     where: {
@@ -10,7 +18,18 @@ exports.show = async (req, res) => {
   return res.json(data);
 };
 
-exports.index = async (req, res) => {
-  const data = await prisma.post.findMany();
-  return res.json(data);
+// create a new post
+exports.create = async (req, res) => {
+  const data = req.body;
+  data.slug = kebabCase(data.title);
+  const newPost = await prisma.post.create({
+    data: {
+      title: data.title,
+      slug: data.slug,
+      image: data.image,
+      content: data.content,
+      published: data.published,
+    },
+  });
+  res.json(newPost);
 };

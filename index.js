3ba142f25fb3d4b9534e99062c8a22531288a866
KevinZucca <kev.94@live.it>
2023-11-23 14:46:10 +0100

    feat: index and show routes and function with async await

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
+const postsRouter = require("./routers/posts");
 
 app.use(express.json());
+
+// routes
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port} `);
